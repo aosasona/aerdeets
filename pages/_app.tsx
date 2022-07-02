b3a6ae2b1b5a953006886a9dd2e11a4c57e6ff23
Ayodeji O <osasonaayodeji1@gmail.com>
2022-07-02 17:20:36 +0100

    perf: added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import "../styles/globals.css";
-import "../styles/main.css";
-import "../styles/nprogress.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { GlobalProvider } from "@/context/GlobalContext";
 
+import "../styles/globals.css";
+import "../styles/main.css";
+import "../styles/nprogress.css";
+
 import Router from "next/router";
 import nProgress from "nprogress";
+import Script from "next/script";
 
 //Router events
 Router.events.on("routeChangeStart", nProgress.start);
@@ -14,9 +17,26 @@ Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GlobalProvider>
-      <Component {...pageProps} />
-    </GlobalProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="main-script" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <GlobalProvider>
+        <Component {...pageProps} />
+      </GlobalProvider>
+    </>
   );
 }
 
